e066d36f6cb92aacc9728cb849378f56dedb3a5d
Amir-Mallek <amir.mallek@insat.ucar.tn>
2025-12-25 10:10:43 +0100

    feat: add mock course items and css button

--- src/app/shared/services/course-service.ts
+++ src/app/shared/services/course-service.ts
@@ -1,6 +1,8 @@
 import { Injectable, model } from '@angular/core';
 import { Course } from '@models/course.model';
 import mockCourses from '@assets/mock-courses.json';
+import mockCoursesItems from '@assets/mock-course-items.json';
+import { CourseItem } from '@models/course-item.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +10,8 @@ import mockCourses from '@assets/mock-courses.json';
 export class CourseService {
   getCourses(): Course[] {
     return mockCourses as Course[];
+  }
+  getCourseItems(courseId: string): CourseItem[] {
+    return mockCoursesItems as CourseItem[];
   }
 }
