be3ba1f8af0c6e6d4711f95d0c2975dbcd7710e9
Hsan <hsan.ellouze@insat.ucar.tn>
2025-12-26 11:47:10 +0100

    feat: add duration and price pipes

--- src/app/shared/components/course-card/course-card.ts
+++ src/app/shared/components/course-card/course-card.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Course } from '@models/course.model';
+import { DurationPipe } from '@pipes/DurationPipe/duration.pipe';
+import { PricePipe } from '@pipes/PricePipe/price-pipe';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DurationPipe, PricePipe],
   templateUrl: './course-card.html',
   styleUrls: ['./course-card.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
