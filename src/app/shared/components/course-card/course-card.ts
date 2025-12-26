6fed242bc888d22acc0d845571df69c6c60d74fe
Hsan <hsan.ellouze@insat.ucar.tn>
2025-12-26 11:56:18 +0100

    feat: add trancate pipe

--- src/app/shared/components/course-card/course-card.ts
+++ src/app/shared/components/course-card/course-card.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Course } from '@models/course.model';
 import { DurationPipe } from '@pipes/DurationPipe/duration.pipe';
 import { PricePipe } from '@pipes/PricePipe/price-pipe';
+import { TruncatePipe } from '@pipes/TruncatePipe/truncate-pipe';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
-  imports: [CommonModule, DurationPipe, PricePipe],
+  imports: [CommonModule, DurationPipe, PricePipe, TruncatePipe],
   templateUrl: './course-card.html',
   styleUrls: ['./course-card.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
